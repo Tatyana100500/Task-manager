356c6e0afdbd4612698000901daaa3167fd8fabe
Tatiana Chernishova <chernishovate@mail.ru>
2021-05-12 15:14:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,162 +1,135 @@
 // @ts-check
-import _ from 'lodash';
-import dotenv from 'dotenv';
-import path from 'path';
 import fastify from 'fastify';
 import fastifyStatic from 'fastify-static';
+import _ from 'lodash';
 import fastifyErrorPage from 'fastify-error-page';
-import pointOfView from 'point-of-view';
 import fastifyFormbody from 'fastify-formbody';
 import fastifySecureSession from 'fastify-secure-session';
-import fastifyPassport from 'fastify-passport';
-import fastifySensible from 'fastify-sensible';
 import fastifyFlash from 'fastify-flash';
-import { plugin as fastifyReverseRoutes } from 'fastify-reverse-routes';
 import fastifyMethodOverride from 'fastify-method-override';
 import fastifyObjectionjs from 'fastify-objectionjs';
+import fastifyReverseRoutes from 'fastify-reverse-routes';
 import qs from 'qs';
-import Pug from 'pug';
+import dotenv from 'dotenv';
 import i18next from 'i18next';
-import ru from './locales/ru.js';
-// @ts-ignore
-import webpackConfig from '../webpack.config.babel.js';
+import path from 'path';
+import pointOfView from 'point-of-view';
+import Pug from 'pug';
 import Rollbar from 'rollbar';
 import fastifyAuth from 'fastify-auth';
-//import createError from 'http-errors';
-import addRoutes from './routes/index.js';
-import getHelpers from './helpers/index.js';
-import knexConfig from '../knexfile.js';
-import models from './models/index.js';
-import FormStrategy from './lib/passportStrategies/FormStrategy.js';
+import createError from 'http-errors';
+import ru from './locales/ru';
+import addRoutes from './routes';
+import knexConfig from '../knexfile';
+import models from './models';
 
 dotenv.config();
+
 const mode = process.env.NODE_ENV || 'development';
-const isProduction = mode === 'production';
-const isDevelopment = mode === 'development';
 const rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR,
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
 
-const setUpViews = (app) => {
-  const { devServer } = webpackConfig;
-  const devHost = `http://${devServer.host}:${devServer.port}`;
-  const domain = isDevelopment ? devHost : '';
-  const helpers = getHelpers(app);
+const setupFrontEnd = (app) => {
+  i18next.init({
+    lng: 'ru',
+    debug: mode === 'development',
+    resources: {
+      ru,
+    },
+  });
+
   app.register(pointOfView, {
     engine: {
       pug: Pug,
     },
     includeViewExtension: true,
+    root: path.join(__dirname, 'views'),
     defaultContext: {
-      ...helpers,
-      assetPath: (filename) => `${domain}/assets/${filename}`,
+      t(key) {
+        return i18next.t(key);
+      },
+      _,
+      isSigned: app.isSigned,
+      currentUser: app.currentUser,
+      urlFor(...args) {
+        return app.reverse(...args);
+      },
     },
-    templates: path.join(__dirname, '..', 'server', 'views'),
+    options: {
+      basedir: path.join(__dirname, 'views'),
+    },
   });
 
   app.decorateReply('render', function render(viewPath, locals) {
     this.view(viewPath, { ...locals, reply: this });
   });
-};
 
-const setUpStaticAssets = (app) => {
-  const pathPublic = isProduction
-    ? path.join(__dirname, '..', 'public')
-    : path.join(__dirname, '..', 'dist', 'public');
   app.register(fastifyStatic, {
-    root: pathPublic,
-    prefix: '/assets/',
-  });
-};
-
-const setupLocalization = () => {
-  i18next
-    .init({
-      lng: 'ru',
-      fallbackLng: 'en',
-      debug: isDevelopment,
-      resources: {
-        ru,
-      },
-    });
-};
-
-const addHooks = (app) => {
-  app.addHook('preHandler', async (req, reply) => {
-    reply.locals = {
-      isAuthenticated: () => req.isAuthenticated(),
-    };
+    root: process.env !== 'production'
+      ? path.join(__dirname, '..', 'dist', 'public')
+      : path.join(__dirname, 'public'),
   });
 };
 
 const registerPlugins = (app) => {
-  app.register(fastifySensible);
-  app.register(fastifyErrorPage);
-  app.register(fastifyReverseRoutes);
+  app.register(fastifyReverseRoutes.plugin);
   app.register(fastifyFormbody, { parser: qs.parse });
   app.register(fastifySecureSession, {
-    secret: process.env.SESSION_KEY,
+    cookieName: 'session',
+    secret: process.env.APPKEY,
     cookie: {
       path: '/',
     },
   });
   app.register(fastifyFlash);
-  app.register(fastifyAuth);
-  fastifyPassport.registerUserDeserializer(
-    (user) => app.objection.models.user.query().findById(user.id),
-  );
-  fastifyPassport.registerUserSerializer((user) => Promise.resolve(user));
-  fastifyPassport.use(new FormStrategy('form', app));
-  app.register(fastifyPassport.initialize());
-  app.register(fastifyPassport.secureSession());
-  app.decorate('fp', fastifyPassport);
-  app.decorate('authenticate', (...args) => fastifyPassport.authenticate(
-    'form',
-    {
-      failureRedirect: app.reverse('root'),
-      failureFlash: i18next.t('flash.authError'),
-    },
-  // @ts-ignore
-  )(...args));
-
   app.register(fastifyMethodOverride);
   app.register(fastifyObjectionjs, {
     knexConfig: knexConfig[mode],
     models,
+  });
+  if (process.env.NODE_ENV !== 'production') app.register(fastifyErrorPage);
+};
+
+const addHooks = (app) => {
+  app.decorateRequest('currentUser', null);
+  app.decorateRequest('isSigned', false);
+  app.decorateRequest('createError', createError);
+  app.addHook('preHandler', async (request) => {
+    const userId = request.session.get('userId');
+    if (userId) {
+      request.currentUser = await app.objection.models.user.query().findById(userId);
+      request.isSigned = true;
+    }
   });
 };
 
 export default () => {
   const app = fastify({
     logger: {
-      prettyPrint: isDevelopment,
+      prettyPrint: mode === 'development',
     },
   });
-
+  setupFrontEnd(app);
   registerPlugins(app);
-
-  setupLocalization();
-  setUpViews(app);
-  setUpStaticAssets(app);
-  addRoutes(app);
   addHooks(app);
-  // app.decorate('authCheck', async (request, reply) => {
-  //   if (!request.isSigned) {
-  //     request.flash('error', i18next.t('views.messages.notLogged'));
-  //     reply.redirect(app.reverse('root'));
-  //   }
-  // })
-  //   .register(fastifyAuth)
-  //   .after(() => {
-  //     addRoutes(app);
-  //   });
-  // if (mode === 'production') {
-  //   app.setErrorHandler((error, req, reply) => {
-  //     rollbar.error(error);
-  //     reply.send(error);
-  //   });
-  // }
+  app.decorate('authCheck', async (request, reply) => {
+    if (!request.isSigned) {
+      request.flash('error', i18next.t('views.messages.notLogged'));
+      reply.redirect(app.reverse('root'));
+    }
+  })
+    .register(fastifyAuth)
+    .after(() => {
+      addRoutes(app);
+    });
+  if (mode === 'production') {
+    app.setErrorHandler((error, req, reply) => {
+      rollbar.error(error);
+      reply.send(error);
+    });
+  }
   return app;
 };
