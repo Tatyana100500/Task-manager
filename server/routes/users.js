40b4fd25c4b66167b5796126eade14322e36a9bc
Tatiana Chernishova <chernishovate@mail.ru>
2021-05-28 16:12:50 +0300

    step7

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,12 +6,12 @@ export default (app) => {
   app
     .get('/users', { name: 'users' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
-      reply.render('users/list', { users });
+      reply.render('users/index', { users });
       return reply;
     })
     .get('/users/new', { name: 'newUser' }, (req, reply) => {
       const user = new app.objection.models.user();
-      reply.render('users/edit', { user });
+      reply.render('users/new', { user });
     })
     .post('/users', async (req, reply) => {
       try {
@@ -22,28 +22,8 @@ export default (app) => {
         return reply;
       } catch ({ data }) {
         req.flash('error', i18next.t('flash.users.create.error'));
-        reply.render('users/edit', { user: req.body.data, errors: data });
+        reply.render('users/new', { user: req.body.data, errors: data });
         return reply;
-      }
-    });
-    app.patch('/users/:id', { name: 'updateUser' }, async (request, reply) => {
-      console.log(request);
-      const userId = request.session.get('userId');
-      if (parseInt(request.params.id, 10) !== userId) {
-        request.flash('error', i18next.t('views.pages.users.edit.noAllowed'));
-        reply.redirect(app.reverse('users'));
-      }
-  
-      try {
-        const user = await app.objection.models.user
-          .query()
-          .findById(request.currentUser.id);
-        await user.$query().update(request.body.user);
-        request.flash('success', 'User updated successfuly');
-        reply.redirect(app.reverse('editUser', { id: request.params.id }));
-      } catch (e) {
-        request.flash('error', 'Invalid data');
-        reply.redirect(app.reverse('editUser', { id: request.params.id }));
       }
     });
 };
