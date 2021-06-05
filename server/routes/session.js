f3514ce09aa6381bb651673c825dfbcab45873d7
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-05 08:40:29 +0300

    Update session.js

--- server/routes/session.js
+++ server/routes/session.js
@@ -6,20 +6,18 @@ import debug from 'debug';
 const log = debug('??????????????????????????');
 
 export default (app) => {
-  console.log(app);
   app
   .get('/session/new', { name: 'newSession' }, (req, reply) => {
       const signInForm = {};
-      console.log(reply);
       reply.render('session/new', { signInForm });
     })
   .post('/session', { name: 'session' }, app.fp.authenticate('form', async (req, reply, err, user) => {
-    console.log(req, reply, err, user);
       if (err) {
         return app.httpErrors.internalServerError(err);
       }
       if (!user) {
         const signInForm = req.body.data;
+        console.log("user end data", signInForm, user)
         const errors = {
           email: [{ message: i18next.t('flash.session.create.error') }],
         };
