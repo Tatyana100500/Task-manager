7e0f70178dfe280f6c777971c78b167abd64905c
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-06 18:28:48 +0300

    Update session.js

--- server/routes/session.js
+++ server/routes/session.js
@@ -60,7 +60,7 @@ export default (app) => {
         const errors = {
           email: [{ message: i18next.t('flash.session.create.error') }],
         };
-        return reply.render('/session', { signInForm, errors });
+        return reply.render('/', { signInForm, errors });
       }
       const ass = await req.logIn(user);
       console.log('success', ass);
