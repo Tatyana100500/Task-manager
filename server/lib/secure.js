2245f64c47fc1345d804e68f80d26aad785b5e1e
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-07 13:24:27 +0300

    Update secure.js

--- server/lib/secure.js
+++ server/lib/secure.js
@@ -5,6 +5,12 @@ import crypto from 'crypto';
 /**
  * @param {string} value
  */
-export default (value) => crypto.createHash('sha256')
-  .update(value)
-  .digest('hex');
+//export default (value) => crypto.createHash('sha256')
+//  .update(value)
+ // .digest('hex');
+ export default (value) => {
+  const secret = process.env.CRYPTOWORD;
+  return crypto.createHmac('sha256', secret)
+    .update(value)
+    .digest('hex');
+};
