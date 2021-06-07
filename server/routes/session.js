22b4c120f660b0d2c1eae28fb1a19f6939942b49
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-07 13:01:01 +0300

    Update session.js

--- server/routes/session.js
+++ server/routes/session.js
@@ -11,45 +11,9 @@ export default (app) => {
       const signInForm = {};
       reply.render('session/new', { signInForm });
     })
+/*
   .post('/session', { name: 'session' }, app.fp.authenticate('form', async (req, reply, err, user) => {
     console.log(user, err, req.body);
-    /*try {
-      const [user1] = await app.objection.models.user
-        .query()
-        .select()
-        .where({
-          email: req.body.email,
-        });
-      const password = encrypt(req.body.password);
-      console.log(req.body);
-      if (!user) {
-        const signInForm = req.body.data;
-       
-        const errors = {
-          email: [{ message: i18next.t('flash.session.create.error') }],
-        };
-        console.log("user end data");
-        return reply.render('/session/new', { signInForm, errors });
-        //req.flash('error', 'Bad username or password');
-        //reply.redirect(app.reverse('/session/new'));
-      }
-      if (password === user.passwordDigest) {
-        const ass = await req.logIn(user);
-      console.log(ass);
-      req.flash.now('success', 'Вы залогинены');
-      return reply.redirect(app.reverse('root'));
-        //req.session.set('userId', user.id);
-        //req.flash('success', `Welcome, ${user.firstName}`);
-        //reply.redirect(app.reverse('root'));
-      }
-    } catch (err) {
-      console.log(app.reverse('root'), app.reverse('session'));
-      req.flash('error', i18next.t('flash.session.create.error'));
-      reply.redirect(app.reverse('root'));
-      //return app.httpErrors.internalServerError(err);
-      //return reply.render('session/new', { signInForm, err });
-      //reply.redirect(app.reverse('login'));
-    }*/
       if (err) {
         console.log("http error", app.reverse('root'), app.reverse('session'));
         return app.httpErrors.internalServerError(err);
@@ -66,7 +30,32 @@ export default (app) => {
       console.log('success', ass);
       req.flash('success', 'Вы залогинены');
       return reply.redirect(app.reverse('root'));
-    }))
+    }))*/
+    .post('/session', { name: 'session' }, async (request, reply) => {
+      try {
+        const [user] = await app.objection.models.user
+          .query()
+          .select()
+          .where({
+            email: request.body.email,
+          });
+        const password = encrypt(request.body.password);
+        console.log(user, password);
+        if (!user || password !== user.passwordDigest) {
+          request.flash('error', 'Bad username or password');
+          reply.redirect(app.reverse('session'));
+        }
+        if (password === user.passwordDigest) {
+          request.session.set('userId', user.id);
+          request.flash('success', 'Вы залогинены');
+          reply.redirect(app.reverse('root'));
+        }
+      } catch {
+        console.log('error !!!')
+        request.flash('error', 'Login error!');
+        reply.redirect(app.reverse('session'));
+      }
+    })
   .delete('/session', (req, reply) => {
       req.logOut();
       req.flash('info', i18next.t('flash.session.delete.success'));
