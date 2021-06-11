5e96dbe594c84e8d93ebe3dac0f4ad20663ee677
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-11 13:47:52 +0300

    Update session.js

--- server/routes/session.js
+++ server/routes/session.js
@@ -14,7 +14,7 @@ export default (app) => {
       reply.render('session/new', { signInForm });
     })
   .post('/session', { name: 'session' }, app.fp.authenticate('form', async (req, reply, err, user) => {
-    console.log(reply, err, user);
+    console.log(reply.request, err, user);
       if (err) {
         return app.httpErrors.internalServerError(err);
       }
