0f0ef71ca41fae4782f86e38eac37a9789ea97ff
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-16 10:16:27 +0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,48 @@
 // @ts-check
 
 const path = require('path');
-require('dotenv').config();
 
 const migrations = {
   directory: path.join(__dirname, 'server', 'migrations'),
 };
 
-module.exports = {
-  development: {
-    client: 'sqlite3',
+const { env } = process;
+
+module.exports = () => ({
+  production: {
+    client: env.PROD_DB_TYPE,
     connection: {
-      filename: './database.sqlite',
+      user: env.PROD_DB_USER,
+      password: env.PROD_DB_PASSWORD,
+      database: env.PROD_DB_NAME,
+      host: env.PROD_DB_HOST,
+      port: env.PROD_DB_PORT,
+      database_url: env.PROD_DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    useNullAsDefault: true,
+    migrations,
+  },
+  development: {
+    client: env.DEV_DB_TYPE,
+    connection: {
+      user: env.DEV_DB_USER,
+      password: env.DEV_DB_PASSWORD,
+      database: env.DEV_DB_NAME,
+      host: env.DEV_DB_HOST,
+      port: env.DEV_DB_PORT,
     },
     useNullAsDefault: true,
     migrations,
   },
   test: {
-    client: 'sqlite3',
-    connection: ':memory:',
-    useNullAsDefault: true,
-    migrations,
-  },
-  production: {
-    client: 'sqlite3',
+    client: env.TEST_DB_TYPE,
     connection: {
-      filename: './database.sqlite',
+      filename: env.TEST_DB,
     },
     useNullAsDefault: true,
     migrations,
   },
-};
+});
