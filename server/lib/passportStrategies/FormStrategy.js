dc665f0a808f66e06b92319ab62ea9d7fd262538
Tatiana Chernishova <chernishovate@mail.ru>
2022-01-20 16:35:09 +0300

    Update FormStrategy.js

--- server/lib/passportStrategies/FormStrategy.js
+++ server/lib/passportStrategies/FormStrategy.js
@@ -18,7 +18,7 @@ export default class FormStrategy extends Strategy {
     const email = _.get(request, 'body.data.email', null);
     const password = _.get(request, 'body.data.password', null);
     const { models } = this.app.objection;
-    const user = await models.user.query().findOne( email );
+    const user = await models.user.query().findOne( { email });
     console.log(email, password, user);
     if (user && user.verifyPassword(password)) {
       return this.success(user);
