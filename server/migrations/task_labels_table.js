d2947decd19e714c37de2634ed719de091f3d0ea
Tatiana Chernishova <chernishovate@mail.ru>
2022-01-29 20:39:04 +0300

    Create task_labels_table.js

---
+++ server/migrations/task_labels_table.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+exports.up = (knex) => (
+    knex.schema.createTable('task_labels', (table) => {
+	  table.increments('id').primary();
+      table.integer('labelId');
+      table.integer('taskId');
+    })
+  );
+  
+  exports.down = (knex) => knex.schema.dropTable('task_labels');
