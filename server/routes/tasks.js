7c84adecd3683331f7ddd0a32ac5837ace45b3b2
Tatiana Chernishova <chernishovate@mail.ru>
2022-01-30 08:02:20 +0300

    Update tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -82,7 +82,7 @@ export default (app) => app
       req.flash('error', i18next.t('flash.tasks.create.error'));
       req.errors(error.data);
       req.entity('task', req.body.data);
-      reply.redirect(app.reverse('tasks'));
+      reply.render('tasks', {erors: error.data});
       return reply;
     }
   })
