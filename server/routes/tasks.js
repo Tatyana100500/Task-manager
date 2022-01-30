6944445026568d14e2ad114061f3e6acb5b73571
Tatiana Chernishova <chernishovate@mail.ru>
2022-01-30 10:59:54 +0300

    Update tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -82,7 +82,7 @@ export default (app) => app
       req.errors(error.data);
       req.entity('task', req.body.data);
 	  //reply.send(error);
-	  reply.redirect(app.reverse('tasks', {task: req.body.data, errors: error.data}));
+	  reply.redirect('tasks', {task: req.body.data, errors: error.data});
       //reply.render('tasks/new', {task: req.body.data, errors: error.data});
       return reply;
     }
