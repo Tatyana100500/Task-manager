4d3d946f69216394842ebcdf87d1611c320656c5
Tatiana Chernishova <chernishovate@mail.ru>
2022-02-04 15:35:39 +0300

    step6

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -84,7 +84,7 @@ export default (app) => app
       req.flash('error', i18next.t('flash.tasks.create.error'));
 	  req.errors(error.data);
       req.entity('task', req.body.data);
-	  reply.render('tasks/new', { user: req.body.data, errors: error.data });
+	  reply.render('tasks/new', { task: req.body.data, errors: error.data });
 	  //reply.redirect('tasks/new', {errors: error.data});
       //reply.render(app.reverse('newTask', {errors: error.data}));
       return reply;
